a32d43b87f4dd029da3c4c7230c69fbedd483754
elyse502 <elyseniyibizi502@gmail.com>
2024-09-19 11:06:15 +0200

    Add completed SpecialityMenu Section

--- clientside/src/pages/Home.jsx
+++ clientside/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Headers from '../components/Header'
+import SpecialityMenu from '../components/SpecialityMenu'
 
 const Home = () => {
   return (
     <div>
       <Headers />
+      <SpecialityMenu />
     </div>
   )
 }
