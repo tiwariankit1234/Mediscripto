7811632ceeb3fb4af80c3c613e965ac326f29a4c
elyse502 <elyseniyibizi502@gmail.com>
2024-09-23 09:44:07 +0200

    Implement Top Doctors Section

--- clientside/src/pages/Home.jsx
+++ clientside/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Headers from '../components/Header'
 import SpecialityMenu from '../components/SpecialityMenu'
+import TopDoctors from '../components/TopDoctors'
 
 const Home = () => {
   return (
     <div>
       <Headers />
       <SpecialityMenu />
+      <TopDoctors />
     </div>
   )
 }
