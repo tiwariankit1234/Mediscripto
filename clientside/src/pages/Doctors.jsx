37ffcd540135595687413f2fc036a9c97d526347
elyse502 <elyseniyibizi502@gmail.com>
2024-09-27 08:34:36 +0200

    Add Filters Menu to Doctors Page for Mobile Devices

--- clientside/src/pages/Doctors.jsx
+++ clientside/src/pages/Doctors.jsx
@@ -5,6 +5,8 @@ import { AppContext } from "../context/AppContext";
 const Doctors = () => {
   const { speciality } = useParams();
   const [filterDoc, setFilterDoc] = useState([]);
+  const [showFilter, setShowFilter] = useState(false);
+
   const navigate = useNavigate();
 
   const { doctors } = useContext(AppContext);
@@ -25,7 +27,19 @@ const Doctors = () => {
     <div>
       <p className="text-gray-600">Browse through the doctors specialist.</p>
       <div className="flex flex-col sm:flex-row items-start gap-5 mt-5">
-        <div className="flex flex-col gap-4 text-sm text-gray-600">
+        <button
+          className={`py-1 px-3 border rounded text-sm transition-all sm:hidden ${
+            showFilter ? "bg-primary text-white" : ""
+          }`}
+          onClick={() => setShowFilter((prev) => !prev)}
+        >
+          Filters
+        </button>
+        <div
+          className={`flex-col gap-4 text-sm text-gray-600 ${
+            showFilter ? "flex" : "hidden sm:flex"
+          }`}
+        >
           <p
             onClick={() =>
               speciality === "General physician"
