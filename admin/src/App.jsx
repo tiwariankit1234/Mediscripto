503a28fd3c2e990c4542fe5c43d40d669dabf22c
elyse502 <elyseniyibizi502@gmail.com>
2024-10-07 19:52:12 +0200

    Implement Admin Login feature

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useContext } from "react";
+import Login from "./pages/Login";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { AdminContext } from "./context/AdminContext";
 
 const App = () => {
-  return (
-    <div>
-      App
-    </div>
-  )
-}
+  const { aToken } = useContext(AdminContext);
 
-export default App
+  return aToken ? (
+    <div>
+      <ToastContainer />
+    </div>
+  ) : (
+    <>
+      <Login />
+      <ToastContainer />
+    </>
+  );
+};
+
+export default App;
