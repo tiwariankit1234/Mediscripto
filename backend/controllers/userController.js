d5057059e345a864ee39a6d6221e5fb7bd762cac
elyse502 <elyseniyibizi502@gmail.com>
2024-10-12 10:24:08 +0200

    Create User Controller to get user profile data

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -68,4 +68,17 @@ const loginUser = async (req, res) => {
   }
 };
 
-export { registerUser, loginUser };
+// API to get user profile data
+const getProfile = async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const useData = await userModel.findById(userId).select("-password");
+
+    res.json({ success: true, user: useData });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export { registerUser, loginUser, getProfile };
