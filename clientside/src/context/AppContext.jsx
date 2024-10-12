aa179ee7f473590880130768cd5486b140b51e68
elyse502 <elyseniyibizi502@gmail.com>
2024-10-12 09:56:55 +0200

    Create Patient Login and Registration system

--- clientside/src/context/AppContext.jsx
+++ clientside/src/context/AppContext.jsx
@@ -7,12 +7,11 @@ export const AppContext = createContext();
 const AppContextProvider = (props) => {
   const currencySymbol = "$";
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
-  const [doctors, setDoctors] = useState([]);
 
-  const value = {
-    doctors,
-    currencySymbol,
-  };
+  const [doctors, setDoctors] = useState([]);
+  const [token, setToken] = useState(
+    localStorage.getItem("token") ? localStorage.getItem("token") : false
+  );
 
   const getDoctorsData = async () => {
     try {
@@ -26,6 +25,14 @@ const AppContextProvider = (props) => {
       console.log(error);
       toast.error(error.message);
     }
+  };
+
+  const value = {
+    doctors,
+    currencySymbol,
+    token,
+    setToken,
+    backendUrl,
   };
 
   useEffect(() => {
