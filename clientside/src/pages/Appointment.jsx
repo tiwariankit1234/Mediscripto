d01770085b5b41bc2fa2b7e1f65789a9ecffd8d2
elyse502 <elyseniyibizi502@gmail.com>
2024-10-14 13:02:16 +0200

    Hide Entries that is already Booked!

--- clientside/src/pages/Appointment.jsx
+++ clientside/src/pages/Appointment.jsx
@@ -59,11 +59,26 @@ const Appointment = () => {
           minute: "2-digit",
         });
 
-        // add slot to array
-        timeSlots.push({
-          datetime: new Date(currentDate),
-          time: formattedTime,
-        });
+        let day = currentDate.getDate();
+        let month = currentDate.getMonth() + 1;
+        let year = currentDate.getFullYear();
+
+        const slotDate = day + "_" + month + "_" + year;
+        const slotTime = formattedTime;
+
+        const isSlotAvailable =
+          docInfo.slots_booked[slotDate] &&
+          docInfo.slots_booked[slotDate].includes(slotTime)
+            ? false
+            : true;
+
+        if (isSlotAvailable) {
+          // add slot to array
+          timeSlots.push({
+            datetime: new Date(currentDate),
+            time: formattedTime,
+          });
+        }
 
         // Increment current time by 30 minutes
         currentDate.setMinutes(currentDate.getMinutes() + 30);
