48dfff9abe764da4212527f65a272a54b06f783c
elyse502 <elyseniyibizi502@gmail.com>
2024-10-17 17:34:22 +0200

    Display appointments in Doctor panel

--- admin/src/pages/Doctor/DoctorAppointments.jsx
+++ admin/src/pages/Doctor/DoctorAppointments.jsx
@@ -1,8 +1,12 @@
 import React, { useContext, useEffect } from "react";
 import { DoctorContext } from "../../context/DoctorContext";
+import { AppContext } from "../../context/AppContext";
+import { assets } from "../../assets/assets";
 
 const DoctorAppointments = () => {
   const { dToken, appointments, getAppointments } = useContext(DoctorContext);
+
+  const { calculateAge, slotDateFormat, currency } = useContext(AppContext);
 
   useEffect(() => {
     if (dToken) {
@@ -10,7 +14,65 @@ const DoctorAppointments = () => {
     }
   }, [dToken]);
 
-  return <div>DoctorAppointments</div>;
+  return (
+    <div className="w-full max-w-6xl m-5">
+      <p className="mb-3 text-lg font-medium">All Appointments</p>
+
+      <div className="bg-white border rounded text-sm max-h-[80vh] min-h-[50vh]   ">
+        <div className="max-sm:hidden grid grid-cols-[0.5fr_2fr_1fr_1fr_3fr_1fr_1fr] gap-1 py-3 px-6 border-b">
+          <p>#</p>
+          <p>Patient</p>
+          <p>Payment</p>
+          <p>Age</p>
+          <p>Date & Time</p>
+          <p>Fees</p>
+          <p>Action</p>
+        </div>
+
+        {appointments.map((item, index) => (
+          <div
+            className="flex flex-wrap justify-between max-sm:gap-5 max-sm:text-base sm:grid grid-cols-[0.5fr_2fr_1fr_1fr_3fr_1fr_1fr] gap-1 items-center text-gray-500 py-3 px-6 border-b hover:bg-gray-50"
+            key={index}
+          >
+            <p className="max-sm:hidden">{index + 1}</p>
+            <div className="flex items-center gap-2">
+              <img
+                className="w-8 rounded-full"
+                src={item.userData.image}
+                alt=""
+              />{" "}
+              <p>{item.userData.name}</p>
+            </div>
+            <div>
+              <p className="text-xs inline border border-primary px-2 rounded-full">
+                {item.payment ? "Onlone" : "CASH"}
+              </p>
+            </div>
+            <p className="max-sm:hidden">{calculateAge(item.userData.dob)}</p>
+            <p>
+              {slotDateFormat(item.slotDate)}, {item.slotTime}
+            </p>
+            <p>
+              {currency}
+              {item.amount}
+            </p>
+            <div className="flex">
+              <img
+                className="w-10 cursor-pointer"
+                src={assets.cancel_icon}
+                alt=""
+              />
+              <img
+                className="w-10 cursor-pointer"
+                src={assets.tick_icon}
+                alt=""
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default DoctorAppointments;
