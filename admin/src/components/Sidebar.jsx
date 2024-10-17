5a4d9ab4d443ba8cde3c50efcd4c30564fecc277
elyse502 <elyseniyibizi502@gmail.com>
2024-10-17 16:53:22 +0200

    Optimize UI Design for small screen on the side bar of both Admin and Doctor panels

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -21,7 +21,7 @@ const Sidebar = () => {
             to={"/admin-dashboard"}
           >
             <img src={assets.home_icon} alt="" />
-            <p>Dashboard</p>
+            <p className="hidden md:block">Dashboard</p>
           </NavLink>
 
           <NavLink
@@ -33,7 +33,7 @@ const Sidebar = () => {
             to={"/all-appointments"}
           >
             <img src={assets.appointment_icon} alt="" />
-            <p>Appointments</p>
+            <p className="hidden md:block">Appointments</p>
           </NavLink>
 
           <NavLink
@@ -45,7 +45,7 @@ const Sidebar = () => {
             to={"/add-doctor"}
           >
             <img src={assets.add_icon} alt="" />
-            <p>Add Doctor</p>
+            <p className="hidden md:block">Add Doctor</p>
           </NavLink>
 
           <NavLink
@@ -57,7 +57,7 @@ const Sidebar = () => {
             to={"/doctor-list"}
           >
             <img src={assets.people_icon} alt="" />
-            <p>Doctors List</p>
+            <p className="hidden md:block">Doctors List</p>
           </NavLink>
         </ul>
       )}
@@ -73,7 +73,7 @@ const Sidebar = () => {
             to={"/doctor-dashboard"}
           >
             <img src={assets.home_icon} alt="" />
-            <p>Dashboard</p>
+            <p className="hidden md:block">Dashboard</p>
           </NavLink>
 
           <NavLink
@@ -85,7 +85,7 @@ const Sidebar = () => {
             to={"/doctor-appointments"}
           >
             <img src={assets.appointment_icon} alt="" />
-            <p>Appointments</p>
+            <p className="hidden md:block">Appointments</p>
           </NavLink>
 
           <NavLink
@@ -97,7 +97,7 @@ const Sidebar = () => {
             to={"/doctor-profile"}
           >
             <img src={assets.people_icon} alt="" />
-            <p>Profile</p>
+            <p className="hidden md:block">Profile</p>
           </NavLink>
         </ul>
       )}
