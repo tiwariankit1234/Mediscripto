8838ac0fa3f5634f7bba0e03c5074617eb71104c
elyse502 <elyseniyibizi502@gmail.com>
2024-10-21 08:48:40 +0200

    Create Doctor Dashboard API to get dashboard data for doctor panel

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -105,6 +105,42 @@ const appointmentCancel = async (req, res) => {
   }
 };
 
+// API to get dashboard data for doctor panel
+const doctorDashboard = async (req, res) => {
+  try {
+    const { docId } = req.body;
+    const appointments = await appointmentModel.find({ docId });
+
+    let earnings = 0;
+
+    appointments.map((item) => {
+      if (item.isCompleted || item.payment) {
+        earnings += item.amount;
+      }
+    });
+
+    let patients = [];
+
+    appointments.map((item) => {
+      if (!patients.includes(item.userId)) {
+        patients.push(item.userId);
+      }
+    });
+
+    const dashData = {
+      earnings,
+      appointments: appointments.length,
+      patients: patients.length,
+      latestAppointments: appointments.reverse().slice(0, 5),
+    };
+
+    res.json({ success: true, dashData });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
 export {
   changeAvailability,
   doctorList,
@@ -112,4 +148,5 @@ export {
   appointmentsDoctor,
   appointmentCancel,
   appointmentComplete,
+  doctorDashboard,
 };
--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -5,6 +5,7 @@ import {
   appointmentsDoctor,
   appointmentComplete,
   appointmentCancel,
+  doctorDashboard
 } from "../controllers/doctorController.js";
 import authDoctor from "../middlewares/authDoctor.js";
 
@@ -15,5 +16,6 @@ doctorRouter.post("/login", loginDoctor);
 doctorRouter.get("/appointments", authDoctor, appointmentsDoctor);
 doctorRouter.post("/complete-appointment", authDoctor, appointmentComplete);
 doctorRouter.post("/cancel-appointment", authDoctor, appointmentCancel);
+doctorRouter.get('/dashboard', authDoctor, doctorDashboard);
 
 export default doctorRouter;
